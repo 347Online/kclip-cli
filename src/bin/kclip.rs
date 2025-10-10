a1b390070b9837c0c026171c3c7f3269801591b3
347Online | Katie Janzen <katiejanzen@347online.me>
2025-10-10 16:18:22 -0500

    App struct container for mutable program state

--- src/bin/kclip.rs
+++ src/bin/kclip.rs
@@ -6,31 +6,6 @@ use std::io::{BufRead, Write, stdin, stdout};
 use std::path::{Path, PathBuf};
 use symlink::symlink_file;
 
-fn copy(cb: &mut Clipboard) -> anyhow::Result<()> {
-    let text = stdin()
-        .lock()
-        .lines()
-        .collect::<Result<String, _>>()
-        .context("Failed to read from stdin")?;
-
-    cb.set_text(text)
-        .context("Failed write content to clipboard")?;
-
-    Ok(())
-}
-
-fn paste(cb: &mut Clipboard) -> anyhow::Result<()> {
-    let text = match cb.get_text() {
-        Ok(x) => x,
-        Err(arboard::Error::ContentNotAvailable) => return Ok(()),
-        Err(err) => Err(err).context("Failed to read contents of clipboard")?,
-    };
-
-    write!(stdout().lock(), "{text}").context("Failed to write to stdout")?;
-
-    Ok(())
-}
-
 macro_rules! primary_commands {
     ($prefix:expr) => {
         [
@@ -47,123 +22,165 @@ macro_rules! primary_commands {
     };
 }
 
-macro_rules! alias_prefix {
-    () => {
-        "kc"
-    };
+struct App {
+    cb: Clipboard,
+    cli: Command,
+    aliased_commands: [Command; 3],
 }
 
-fn install(target: &Path) -> anyhow::Result<()> {
-    let src = std::env::current_exe()?;
+impl App {
+    pub fn new() -> anyhow::Result<Self> {
+        let cb = Clipboard::new().context("Failed to access clipboard")?;
+        let app_dir = app_path!().to_string();
 
-    let commands = primary_commands!(alias_prefix!());
-    let total = commands.len();
-    let mut succeeded = 0;
+        let primary_commands = primary_commands!();
+        let aliased_commands = primary_commands!("kc");
 
-    for cmd in commands {
-        let alias = target.join(cmd.get_name());
+        let cli = command!("kclip")
+            .multicall(true)
+            .propagate_version(true)
+            .subcommands(&aliased_commands)
+            .subcommand(
+                command!("kclip")
+                    .arg_required_else_help(true)
+                    .subcommand_help_heading("COMMANDS")
+                    .subcommands(&primary_commands)
+                    .subcommand(
+                        Command::new("install")
+                            .about("Install symlink aliases")
+                            .arg(
+                                Arg::new("TARGET")
+                                    .help("Install symlink aliases to specified target")
+                                    .value_name("TARGET")
+                                    .default_value(&app_dir)
+                                    .value_parser(value_parser!(PathBuf)),
+                            ),
+                    )
+                    .subcommand(
+                        Command::new("inspect")
+                            .about("Display information about the current clipboard content"),
+                    ),
+            );
 
-        match symlink_file(&src, &alias)
-            .context(format!("Failed to symlink {:?} -> {:?}", &src, alias))
-        {
-            Ok(_) => succeeded += 1,
-            Err(err) => {
-                eprintln!("{err}");
-                if let Some(cause) = err.source() {
-                    eprintln!("   {cause}");
+        Ok(App {
+            cb,
+            cli,
+            aliased_commands,
+        })
+    }
+
+    fn copy(&mut self) -> anyhow::Result<()> {
+        let text = stdin()
+            .lock()
+            .lines()
+            .collect::<Result<String, _>>()
+            .context("Failed to read from stdin")?;
+
+        self.cb
+            .set_text(text)
+            .context("Failed write content to clipboard")?;
+
+        Ok(())
+    }
+
+    fn paste(&mut self) -> anyhow::Result<()> {
+        let text = match self.cb.get_text() {
+            Ok(x) => x,
+            Err(arboard::Error::ContentNotAvailable) => return Ok(()),
+            Err(err) => Err(err).context("Failed to read contents of clipboard")?,
+        };
+
+        write!(stdout().lock(), "{text}").context("Failed to write to stdout")?;
+
+        Ok(())
+    }
+
+    fn install(&self, target: &Path) -> anyhow::Result<()> {
+        let src = std::env::current_exe()?;
+
+        let total = self.aliased_commands.len();
+        let mut succeeded = 0;
+
+        for cmd in &self.aliased_commands {
+            let alias = target.join(cmd.get_name());
+
+            match symlink_file(&src, &alias)
+                .context(format!("Failed to symlink {:?} -> {:?}", &src, alias))
+            {
+                Ok(_) => succeeded += 1,
+                Err(err) => {
+                    eprintln!("{err}");
+                    if let Some(cause) = err.source() {
+                        eprintln!("   {cause}");
+                    }
                 }
             }
         }
+
+        if succeeded == total {
+            println!("All aliases installed successfully");
+        } else {
+            println!("{succeeded}/{total} aliases installed successfully");
+            std::process::exit(1);
+        }
+
+        Ok(())
     }
 
-    if succeeded == total {
-        println!("All aliases installed successfully");
-    } else {
-        println!("{succeeded}/{total} aliases installed successfully");
-        std::process::exit(1);
+    fn clear(&mut self) -> anyhow::Result<()> {
+        Ok(self.cb.clear()?)
     }
 
-    Ok(())
-}
+    fn inspect(&mut self) {
+        let repr = if let Ok(_paths) = self.cb.get().file_list() {
+            "files"
+        } else if let Ok(_image) = self.cb.get().image() {
+            "image"
+        } else if let Ok(_html) = self.cb.get().html() {
+            "html"
+        } else if let Ok(_text) = self.cb.get().text() {
+            "text"
+        } else {
+            "<empty>"
+        };
 
-fn clear(cb: &mut Clipboard) -> anyhow::Result<()> {
-    Ok(cb.clear()?)
-}
+        println!("{repr}");
+    }
 
-fn inspect(cb: &mut Clipboard) {
-    let repr = if let Ok(_paths) = cb.get().file_list() {
-        "files"
-    } else if let Ok(_image) = cb.get().image() {
-        "image"
-    } else if let Ok(_html) = cb.get().html() {
-        "html"
-    } else if let Ok(_text) = cb.get().text() {
-        "text"
-    } else {
-        "<empty>"
-    };
+    pub fn invoke(mut self) -> anyhow::Result<()> {
+        let cli = std::mem::take(&mut self.cli);
+        let matches = cli.get_matches();
 
-    println!("{repr}");
-}
+        let subcommand = matches.subcommand().and_then(|x| {
+            if let ("kclip", cmd) = x {
+                cmd.subcommand()
+            } else {
+                Some(x)
+            }
+        });
 
-fn cli(app_dir: String) -> Command {
-    command!("kclip")
-        .multicall(true)
-        .propagate_version(true)
-        .subcommands(primary_commands!(alias_prefix!()))
-        .subcommand(
-            command!("kclip")
-                .arg_required_else_help(true)
-                .subcommand_help_heading("COMMANDS")
-                .subcommands(primary_commands!())
-                .subcommand(
-                    Command::new("install")
-                        .about("Install symlink aliases")
-                        .arg(
-                            Arg::new("TARGET")
-                                .help("Install symlink aliases to specified target")
-                                .value_name("TARGET")
-                                .default_value(app_dir)
-                                .value_parser(value_parser!(PathBuf)),
-                        ),
-                )
-                .subcommand(
-                    Command::new("inspect")
-                        .about("Display information about the current clipboard content"),
-                ),
-        )
+        match subcommand {
+            Some(("kccopy" | "copy", _)) => self.copy()?,
+            Some(("kcpaste" | "paste", _)) => self.paste()?,
+            Some(("kcclear" | "clear", _)) => self.clear()?,
+            Some(("inspect", _)) => self.inspect(),
+            Some(("install", cmd)) => {
+                let target = cmd
+                    .get_one::<PathBuf>("TARGET")
+                    .expect("TARGET should always have a value");
+
+                self.install(target)?;
+            }
+
+            _ => unreachable!("parser should ensure only valid names are used"),
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> anyhow::Result<()> {
-    let app_dir = app_path!().to_string();
-    let cmd = cli(app_dir);
+    let app = App::new()?;
 
-    let mut cb = Clipboard::new().context("Failed to access clipboard")?;
-
-    let matches = cmd.get_matches();
-    let subcommand = matches.subcommand().and_then(|x| {
-        if let ("kclip", cmd) = x {
-            cmd.subcommand()
-        } else {
-            Some(x)
-        }
-    });
-
-    match subcommand {
-        Some(("kccopy" | "copy", _)) => copy(&mut cb)?,
-        Some(("kcpaste" | "paste", _)) => paste(&mut cb)?,
-        Some(("kcclear" | "clear", _)) => clear(&mut cb)?,
-        Some(("inspect", _)) => inspect(&mut cb),
-        Some(("install", cmd)) => {
-            let target = cmd
-                .get_one::<PathBuf>("TARGET")
-                .expect("TARGET should always have a value");
-
-            install(target)?;
-        }
-
-        _ => unreachable!("parser should ensure only valid names are used"),
-    }
-
-    Ok(())
+    app.invoke()
 }
