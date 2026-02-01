a115b732db0aa43631645f0ec435cf0be1f11339
347Online | Katie Janzen <katiejanzen@347online.me>
2026-02-01 13:54:53 -0600

    Pin to stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
